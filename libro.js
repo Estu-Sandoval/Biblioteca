1e030afa7122b0629b2daf1de0ca65064149d88f
Estu-Sandoval <estebansaul@gmail.com>
2024-10-22 16:38:52 -0300

    Se agregó interfaz gráfica

--- libro.js
+++ libro.js
@@ -1,75 +1,86 @@
 export class Libro {
-    #titulo; #autor; #genero; #editorial; #ISBN; #cant_paginas;
-    constructor(titulo, autor, genero, editorial, ISBN, cant_paginas) {
-        this.#titulo = titulo;
-        this.#autor = autor;
-        this.#genero = genero;
-        this.#editorial = editorial;
-        this.#ISBN = ISBN;
-        this.#cant_paginas = cant_paginas;
-    }
+  #titulo;
+  #autor;
+  #genero;
+  #editorial;
+  #ISBN;
+  #cant_paginas;
+  #id;
+  static #instance = 0;
 
-    // Getters
-    get titulo() {
-        return this.#titulo;
-    }
+  /**
+   * Crea una instancia de la clase Libro.
+   * @param {string} titulo - Título del libro.
+   * @param {string} autor - Autor del libro.
+   * @param {string} genero - Genero del libro.
+   * @param {string} editorial - Editorial del libro.
+   * @param {string} ISBN - ISBN del libro.
+   * @param {number} cant_paginas - Cantidad de páginas del libro.
+   */
+  constructor(titulo, autor, genero, editorial, ISBN, cant_paginas) {
+    this.#id = ++Libro.#instance;
+    this.#titulo = titulo;
+    this.#autor = autor;
+    this.#genero = genero;
+    this.#editorial = editorial;
+    this.#ISBN = ISBN;
+    this.#cant_paginas = cant_paginas;
+  }
 
-    get autor() {
-        return this.#autor;
-    }
+  // Getters
+  get titulo() {
+    return this.#titulo;
+  }
 
-    get genero() {
-        return this.#genero;
-    }
+  get autor() {
+    return this.#autor;
+  }
 
-    get editorial() {
-        return this.#editorial;
-    }
+  get genero() {
+    return this.#genero;
+  }
 
-    get ISBN() {
-        return this.#ISBN;
-    }
+  get editorial() {
+    return this.#editorial;
+  }
 
-    get cant_paginas() {
-        return this.#cant_paginas;
-    }
+  get ISBN() {
+    return this.#ISBN;
+  }
 
-    // Setters
-    set titulo(nuevoTitulo) {
-        this.#titulo = nuevoTitulo;
-    }
+  get cant_paginas() {
+    return this.#cant_paginas;
+  }
 
-    set autor(nuevoAutor) {
-        this.#autor = nuevoAutor;
-    }
+  get id() {
+    return this.#id;
+  }
 
-    set genero(nuevoGenero) {
-        this.#genero = nuevoGenero;
-    }
+  // Setters
+  set titulo(nuevoTitulo) {
+    this.#titulo = nuevoTitulo;
+  }
 
-    set editorial(nuevaEditorial) {
-        this.#editorial = nuevaEditorial;
-    }
+  set autor(nuevoAutor) {
+    this.#autor = nuevoAutor;
+  }
 
-    set ISBN(nuevoISBN) {
-        this.#ISBN = nuevoISBN;
-    }
+  set genero(nuevoGenero) {
+    this.#genero = nuevoGenero;
+  }
 
-    set cant_paginas(nuevaCantPaginas) {
-        this.#cant_paginas = nuevaCantPaginas;
-    }
+  set editorial(nuevaEditorial) {
+    this.#editorial = nuevaEditorial;
+  }
+
+  set ISBN(nuevoISBN) {
+    this.#ISBN = nuevoISBN;
+  }
+
+  set cant_paginas(nuevaCantPaginas) {
+    this.#cant_paginas = nuevaCantPaginas;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 /*         this.#ejemplares_disponibles = 5; */
 
@@ -80,4 +91,3 @@ export class Libro {
 /*     get ejemplares_disponibles() {
         return this.#ejemplares_disponibles;
     } */
-
